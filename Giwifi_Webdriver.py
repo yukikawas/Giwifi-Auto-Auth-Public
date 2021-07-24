0d2477ce8a36dc8bcf94d4fe578f8068ce481919
tinqlo <30965687+TinQlo@users.noreply.github.com>
2021-07-24 18:20:13 +0800

    7.24, final edition (while true方式)

--- Giwifi_Webdriver.py
+++ Giwifi_Webdriver.py
@@ -1,4 +1,5 @@
 # -*- coding:utf-8 -*-
+# !/usr/bin/python3
 
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
@@ -6,34 +7,58 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
+import requests
+import re
 import time
 
+phone = 'your phone'
+pwd = 'your pwd'
+
+
 def GwifiAuth():
-    phone = 'your phone' #change to your phone
-    pwd = 'your pwd' #change to your pwd
     options = Options()
     options.headless = True
-    
+
     wd = webdriver.Firefox(options=options)
     wd.implicitly_wait(10)
     wd.set_page_load_timeout(10)
-    wd.get("link which can redirect to Giwifi login page")  #change to link which can redirect to Giwifi login page
+    wd.get("http://172.16.1.1:8062/redirect")
+    # wd.maximize_window()
 
-    name_input = wd.find_element_by_id('first_name')
+    """这段可以查看selenium的源码,属于smart wait"""
+    name_input = wd.find_element_by_id('first_name')  # 找到用户名的框框
     pass_input = wd.find_element_by_id('first_password')
     login_button = wd.find_element_by_id('first_button')
     name_input.clear()
-    name_input.send_keys(phone)
+    name_input.send_keys(phone)  # 填写用户名
     time.sleep(0.2)
     pass_input.clear()
-    pass_input.send_keys(pwd)
+    pass_input.send_keys(pwd)  # 填写密码
     time.sleep(0.2)
-    login_button.click()
+    login_button.click()  # 点击登录
     wd.implicitly_wait(4)
     wd.quit()
+    print("All Done")
 
-    print ("All Done")
-    exit()
 
-if __name__ == "__main":
-    GwifiAuth()
+def writelog(info):
+    times = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    LogFile = open('GwifiAuth.log', 'a')
+    LogFile.write(times + ' ' + info + '\r\n')
+    LogFile.close()
+
+
+while True:
+    try:
+        loginPage = requests.get('https://baidu.com', timeout=5).text
+        time.sleep(10)
+        pattern = re.compile("STATUS OK")
+        if pattern.search(loginPage) == None:
+            writelog('Auth lost, try to re-Auth')
+            GwifiAuth()
+            print('reconnected')
+            writelog('Auth commpleted!')
+        else:
+            writelog('Already connected')
+    except:
+        pass
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Giwifi-Auto-Auth-Public
 基于Python selenium 的公开版 Giwifi 自动认证脚本  
 
-必须：Firefox浏览器， Geckodriver， selenium  
+必须：Firefox浏览器， Geckodriver， selenium, requests  
 
