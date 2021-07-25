213d69da981cd92b9318b4900d888f2e62b85bb1
tinqlo <30965687+TinQlo@users.noreply.github.com>
2021-07-25 21:14:44 +0800

    7.25, remove private value, use get_info() from config.pwd (first load will create). add geckodriver 0.29

--- Giwifi_Webdriver.py
+++ Giwifi_Webdriver.py
@@ -2,40 +2,34 @@
 # !/usr/bin/python3
 
 from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.by import By
 import requests
 import re
+import json
 import time
 
-phone = 'your phone'
-pwd = 'your pwd'
+global phone, pwd
 
 
-def gwifi_auth():
+def gwifi_auth(phone, pwd):
     options = Options()
     options.headless = True
 
     wd = webdriver.Firefox(options=options)
     wd.implicitly_wait(10)
     wd.set_page_load_timeout(10)
-    wd.get("http://172.16.1.1:8062/redirect")
-    # wd.maximize_window()
+    wd.get("http://down.gwifi.com.cn")
 
-    """这段可以查看selenium的源码,属于smart wait"""
-    name_input = wd.find_element_by_id('first_name')  # 找到用户名的框框
+    name_input = wd.find_element_by_id('first_name')
     pass_input = wd.find_element_by_id('first_password')
     login_button = wd.find_element_by_id('first_button')
     name_input.clear()
-    name_input.send_keys(phone)  # 填写用户名
+    name_input.send_keys(phone)
     time.sleep(0.2)
     pass_input.clear()
-    pass_input.send_keys(pwd)  # 填写密码
+    pass_input.send_keys(pwd)
     time.sleep(0.2)
-    login_button.click()  # 点击登录
+    login_button.click()
     wd.implicitly_wait(4)
     wd.quit()
     print("All Done")
@@ -48,14 +42,42 @@ def writelog(info):
     log_file.close()
 
 
-while True:
+def get_info():
+    try:
+        file = open('config.pwd', 'r')
+        js = file.read()
+        dic = json.loads(js)
+        phone = dic['phone']
+        pwd = dic['pwd']
+    except FileNotFoundError:
+        phone = input('First use, please input your phone number: ')
+        pwd = input('First use, please input your Gwifi password: ')
+        dic = {'phone': phone, 'pwd': pwd}
+        jsfile = json.dumps(dic)
+        file = open('config.pwd', 'w')
+        file.write(jsfile)
+        file.close()
+        file = open('config.pwd', 'r')
+        js = file.read()
+        file.close()
+        dic = json.loads(js)
+        phone = dic['phone']
+        pwd = dic['pwd']
+        print('success!')
+    return phone, pwd
+
+
+while True:  # TODO use daemon instead of while true
+    phone = get_info()[0]
+    pwd = get_info()[1]
+
     try:
         loginPage = requests.get('https://baidu.com', timeout=5).text
         time.sleep(10)
         pattern = re.compile("STATUS OK")
         if pattern.search(loginPage) == None:
             writelog('Auth lost, try to re-Auth')
-            gwifi_auth()
+            gwifi_auth(phone, pwd)
             print('reconnected')
             writelog('Auth completed!')
         else:
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Giwifi-Auto-Auth-Public
-基于Python selenium 的公开版 Giwifi 自动认证脚本  
+基于Python selenium 的 Giwifi 自动认证脚本  
 
-必须：Firefox浏览器， Geckodriver， selenium, requests  
-
-注意：将代码13和14行的your phone与your pwd改为你自己的信息  
+必须：Firefox浏览器本体， Geckodriver， selenium, requests  
+测试环境：Ubuntu 21.04 ARM64 （树莓派）  
+需要在服务器执行  
+    apt install firefox-geckodriver python3 python3-pip  
+    python3 pip install selenium requests
