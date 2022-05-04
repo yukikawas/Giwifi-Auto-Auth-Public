4758b753953698a31963a20231b94ddb76d5811b
TinQlo <orisui@icloud.com>
2022-05-04 21:54:03 +0800

    gwifi_auto_auth 1.0 create windows_edition_upgrade branch and TODO.

--- Giwifi_Webdriver.py
+++ Giwifi_Webdriver.py
@@ -1,6 +1,14 @@
 # -*- coding:utf-8 -*-
 # !/usr/bin/python3
 
+#####################
+# Author: Yuki Sui
+# Date: 2022-5-4
+#####################
+
+# TODO: need rebuild, use protable firefox package --yukikawa
+
+
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 import requests
@@ -20,10 +28,13 @@ def gwifi_auth(phone, pwd):
     wd.implicitly_wait(10)
     wd.set_page_load_timeout(10)
     wd.get("http://down.gwifi.com.cn")
+    # TODO: now gwifi login page has a alert, need process it --yukikawa
 
+    # TODO: need rebuild, use new feature (By.id()) --yukikawa
     name_input = wd.find_element_by_id('first_name')
     pass_input = wd.find_element_by_id('first_password')
     login_button = wd.find_element_by_id('first_button')
+    # TODO: need rebuild, use new feature (By.id()) --yukikawa
     name_input.clear()
     name_input.send_keys(phone)
     time.sleep(0.2)
@@ -32,6 +43,8 @@ def gwifi_auth(phone, pwd):
     time.sleep(0.2)
     login_button.click()
     wd.implicitly_wait(4)
+    # TODO: need use random delay --yukikawa
+
     wd.quit()
     print("All Done")
 
@@ -69,7 +82,7 @@ while True:
         loginPage = requests.get('https://baidu.com', timeout=5).text
         time.sleep(10)
         pattern = re.compile("STATUS OK")
-        if pattern.search(loginPage) == None:
+        if pattern.search(loginPage) is None:
             logfly.write_log('auth', 'fileCLI', 'error', 'Auth lost, try to re-Auth')
             gwifi_auth(phone, pwd)
             print('reconnected')
